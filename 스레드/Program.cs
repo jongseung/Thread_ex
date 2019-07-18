2e0b00bbe23e9eec9318956b9a34066f38ef7c64
jongseung <park930902@gmail.com>
2019-07-18 16:41:09 +0900

    경주마 완

--- 스레드/Program.cs
+++ 스레드/Program.cs
@@ -25,28 +25,91 @@ namespace 스레드
         public int distance, number;
         public bool isFinish;
         //서브스레드로 동작할 달리기 함수 정의
-        public void run()
-        {
+        public void run(object seed)
+        { //seed값은 보편적으로 현재시간으로 해서 랜덤하게 줌
             isFinish = false;
             //달리기
             //Random : 난수를 생성할 때 사용하는 클래스
-            Random random = new Random();
+            Random random = new Random((int)DateTime.Now.Ticks+((int) seed));
             //Random.Next(최대값) : 0~ 최대값 사이의 임의의 숫자를 반환하는 함수
-            for(distance = 0; distance< 1000; distance += random.Next(5))
+            for(distance = 0; distance< 1000; distance += random.Next(40))
             {
-                Thread.Sleep(500);
+                Thread.Sleep(50);
             }
-
             isFinish = true;
-            rank[rank_index] = number;
-            rank_index ++;
+            //도착 후 순위 등록
+            //lock(변수) : 특정 변수를 접근하는 다양한 스레드들의 데이터 입력/변경을 순차적으로 진행하도록 처리하는 문법
+            //ex) 1, 5번 스레드가 거의 동시에 rank변수 접근하는 경우 가장먼저 lock문법에 도착한 스레드가 점유하는 동안 다른 스레드가 대기상태로 정지함.
+            lock (rank)
+            {
+                rank[rank_index] = number;
+                rank_index++;
+            }
+            
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            //시작 종료 여부 다 종료하면 결과 출력
+            int num = 10;
+            //랭크변수 초기화
+            Player.rank = new int[num];
+            Player.rank_index = 0;
+
+            Player[] players = new Player[num];
+            //선수 수만큼 객체 생성
+            for(int i = 0; i <num; i++)
+            {
+                players[i] = new Player();
+                players[i].number = i;
+            }
+
+            Task[] tasks = new Task[num];
+            //서브스레드 - Player객체의 run 메소드
+            for(int i =0; i <num; i++)
+            {
+                tasks[i] = new Task(new Action<object>(players[i].run),i);
+            }
+            Console.WriteLine("시작하려면 엔터키를 누르세요");
+            Console.ReadLine();
+            //서브스레드 시작
+            for(int i = 0; i < num; i++)
+                tasks[i].Start();
+            //반복문- 모든 선수가 도착할때까지 대기
+            for(; ; )
+            {
+                //선수들이 경기를 마쳤느지 확인
+                bool isAllFinsih = true;
+                for(int i =0; i <num; i++)
+                {
+                    if ((isAllFinsih = players[i].isFinish) == false)
+                    {
+                        break;
+                    }
+                }
+                if (isAllFinsih)
+                {
+                    Console.WriteLine("경기종료");
+                    break;
+                }
+                Console.Clear();
+                //Player객체의 distance값을 화면에 출력
+                for(int i=0; i<num; i++)
+                {
+                    Console.WriteLine("{0} : {1}",players[i].number+1,players[i].distance);
+                }
+                Thread.Sleep(100);
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("순위\t번호");
+            Console.WriteLine("-------------------------------------");
+            //순위 출력
+            for(int i = 0; i < num; i++)
+            {
+                Console.WriteLine("{0}\t{1}", i+1, Player.rank[i]+1);
+            }
+
         }
     }
 }
