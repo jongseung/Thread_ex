d200296a88f7cdb4b23042f3be092b25fb7d5ad0
jongseung <park930902@gmail.com>
2019-07-18 15:56:50 +0900

    플레이어 클래스 완성

--- 스레드/Program.cs
+++ 스레드/Program.cs
@@ -19,10 +19,28 @@ namespace 스레드
     class Player
     {
         //정적변수로 순위를 저장하는 배열과 인덱스저장 변수 선언
-
+        public static int[] rank;
+        public static int rank_index;
         //현재 지점, 등번호, 종료 여부
-
+        public int distance, number;
+        public bool isFinish;
         //서브스레드로 동작할 달리기 함수 정의
+        public void run()
+        {
+            isFinish = false;
+            //달리기
+            //Random : 난수를 생성할 때 사용하는 클래스
+            Random random = new Random();
+            //Random.Next(최대값) : 0~ 최대값 사이의 임의의 숫자를 반환하는 함수
+            for(distance = 0; distance< 1000; distance += random.Next(5))
+            {
+                Thread.Sleep(500);
+            }
+
+            isFinish = true;
+            rank[rank_index] = number;
+            rank_index ++;
+        }
     }
     class Program
     {
